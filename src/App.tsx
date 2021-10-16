9222253103cc35ce232cabf10ec50be340fa22f5
Rahsheen Porter <rahsheen.porter@gmail.com>
2021-10-16 14:21:15 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import "./App.css";
+
+const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon";
+const POKEMON_SPECIES_URL = "https://pokeapi.co/api/v2/pokemon-species";
+
+type Pokemon = {
+  name: string;
+  url: string;
+  sprites: any;
+};
+
+function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+  return <img src={pokemon?.sprites?.frontdefault} alt={pokemon.name} />;
+}
 
 function App() {
+  const [species, setSpecies] = useState<Pokemon[]>([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState<Pokemon[]>([]);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setSearchText(e.target?.value);
+
+  useEffect(() => {
+    fetch(POKEMON_SPECIES_URL)
+      .then((r) => r.json())
+      .then(({ results }) => setSpecies(results))
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    const filteredSpecies = searchText
+      ? species.filter(({ name }) => name.startsWith(searchText))
+      : species;
+
+    const promises = filteredSpecies.map(({ name }) =>
+      fetch(`${POKEMON_URL}/${name}`)
+        .then((r) => r.json())
+        .catch(console.error)
+    );
+
+    Promise.all(promises).then(setSearchResults).catch(console.error);
+  }, [searchText, species]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={onChange} value={searchText} />
+      {searchResults.map((p) => (
+        <PokemonCard key={p.name} pokemon={p} />
+      ))}
     </div>
   );
 }
